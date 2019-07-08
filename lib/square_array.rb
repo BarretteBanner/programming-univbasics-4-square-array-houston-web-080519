ee7aec9f613c870310e9c3635300c6c1cefddc1a
Barrette Banner <barrettebanner@gmail.com>
2019-07-08 17:44:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter = 0 
   newArray = []
   while counter < array.length do
-   newArray.push(Math.(array[counter]))
+   newArray.push(array[counter]**2)
    counter += 1 
   end 
   return newArray
